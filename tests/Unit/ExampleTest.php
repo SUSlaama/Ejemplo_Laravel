1bbb3509421ab74ed0e17de47739aa04d8228693
gerardoelcrack2 <conejillolopz@gmail.com>
2025-12-06 18:51:32 -0600

    Prueba de commit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,7 +13,7 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example. Prueba que true sea true
+     * A basic test example.
      */
     public function test_that_true_is_true(): void
     {
